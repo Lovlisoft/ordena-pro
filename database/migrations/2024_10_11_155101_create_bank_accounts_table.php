9245de2f0f99fd9a838635c3bedb61ba4a60c12d
Abimael Espinoza <abimael@lovlisoft.com>
2024-10-11 11:44:19 -0500

    [V8-49] Add banks modeling

---
+++ database/migrations/2024_10_11_155101_create_bank_accounts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Crater\Models\Bank;
+use Crater\Models\Company;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBankAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bank_accounts', function (Blueprint $table) {
+            $table->id();
+            $table->string('alias');
+            $table->string('account');
+            $table->foreignIdFor(Bank::class);
+            $table->foreignIdFor(Company::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bank_accounts');
+    }
+}
