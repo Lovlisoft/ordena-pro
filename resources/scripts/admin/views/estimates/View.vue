bfed8e14a5ae4e2e7581527eeb65d5d8503332a9
Abimael Espinoza <abimael@lovlisoft.com>
2024-11-01 16:14:50 -0600

    wip

--- resources/scripts/admin/views/estimates/View.vue
+++ resources/scripts/admin/views/estimates/View.vue
@@ -1,6 +1,6 @@
 <template>
   <SendEstimateModal @update="updateSentEstimate" />
-  <BasePage v-if="estimateData" class="xl:pl-96 xl:ml-8">
+  <BasePage v-if="estimateData">
     <BasePageHeader :title="pageTitle">
       <template #actions>
         <div class="mr-3 text-sm">
@@ -35,230 +35,18 @@
       </template>
     </BasePageHeader>
 
-    <!-- Sidebar -->
-    <div
-      class="
-        fixed
-        top-0
-        left-0
-        hidden
-        h-full
-        pt-16
-        pb-[6.4rem]
-        ml-56
-        bg-white
-        xl:ml-64
-        w-88
-        xl:block
-      "
-    >
-      <div
-        class="
-          flex
-          items-center
-          justify-between
-          px-4
-          pt-8
-          pb-2
-          border border-gray-200 border-solid
-          height-full
-        "
-      >
-        <div class="mb-6">
-          <BaseInput
-            v-model="searchData.searchText"
-            :placeholder="$t('general.search')"
-            type="text"
-            variant="gray"
-            @input="onSearched()"
-          >
-            <template #right>
-              <BaseIcon name="SearchIcon" class="text-gray-400" />
-            </template>
-          </BaseInput>
-        </div>
-
-        <div class="flex mb-6 ml-3" role="group" aria-label="First group">
-          <BaseDropdown
-            class="ml-3"
-            position="bottom-start"
-            width-class="w-45"
-            position-class="left-0"
-          >
-            <template #activator>
-              <BaseButton size="md" variant="gray">
-                <BaseIcon name="FilterIcon" />
-              </BaseButton>
-            </template>
-
-            <div
-              class="
-                px-4
-                py-1
-                pb-2
-                mb-1 mb-2
-                text-sm
-                border-b border-gray-200 border-solid
-              "
-            >
-              {{ $t('general.sort_by') }}
-            </div>
-
-            <BaseDropdownItem class="flex px-4 py-2 cursor-pointer">
-              <BaseInputGroup class="-mt-3 font-normal">
-                <BaseRadio
-                  id="filter_estimate_date"
-                  v-model="searchData.orderByField"
-                  :label="$t('reports.estimates.estimate_date')"
-                  size="sm"
-                  name="filter"
-                  value="estimate_date"
-                  @update:modelValue="onSearched"
-                />
-              </BaseInputGroup>
-            </BaseDropdownItem>
-
-            <BaseDropdownItem class="flex px-4 py-2 cursor-pointer">
-              <BaseInputGroup class="-mt-3 font-normal">
-                <BaseRadio
-                  id="filter_due_date"
-                  v-model="searchData.orderByField"
-                  :label="$t('estimates.due_date')"
-                  value="expiry_date"
-                  size="sm"
-                  name="filter"
-                  @update:modelValue="onSearched"
-                />
-              </BaseInputGroup>
-            </BaseDropdownItem>
-
-            <BaseDropdownItem class="flex px-4 py-2 cursor-pointer">
-              <BaseInputGroup class="-mt-3 font-normal">
-                <BaseRadio
-                  id="filter_estimate_number"
-                  v-model="searchData.orderByField"
-                  :label="$t('estimates.estimate_number')"
-                  value="estimate_number"
-                  size="sm"
-                  name="filter"
-                  @update:modelValue="onSearched"
-                />
-              </BaseInputGroup>
-            </BaseDropdownItem>
-          </BaseDropdown>
-
-          <BaseButton class="ml-1" size="md" variant="gray" @click="sortData">
-            <BaseIcon v-if="getOrderBy" name="SortAscendingIcon" />
-            <BaseIcon v-else name="SortDescendingIcon" />
-          </BaseButton>
-        </div>
-      </div>
-
-      <div
-        ref="estimateListSection"
-        class="
-          h-full
-          overflow-y-scroll
-          border-l border-gray-200 border-solid
-          base-scroll
-        "
-      >
-        <div v-for="(estimate, index) in estimateList" :key="index">
-          <router-link
-            v-if="estimate"
-            :id="'estimate-' + estimate.id"
-            :to="`/admin/estimates/${estimate.id}/view`"
-            :class="[
-              'flex justify-between side-estimate p-4 cursor-pointer hover:bg-gray-100 items-center border-l-4 border-transparent',
-              {
-                'bg-gray-100 border-l-4 border-primary-500 border-solid':
-                  hasActiveUrl(estimate.id),
-              },
-            ]"
-            style="border-bottom: 1px solid rgba(185, 193, 209, 0.41)"
-          >
-            <div class="flex-2">
-              <BaseText
-                :text="estimate.customer.name"
-                :length="30"
-                class="
-                  pr-2
-                  mb-2
-                  text-sm
-                  not-italic
-                  font-normal
-                  leading-5
-                  text-black
-                  capitalize
-                  truncate
-                "
-              />
-
-              <div
-                class="
-                  mt-1
-                  mb-2
-                  text-xs
-                  not-italic
-                  font-medium
-                  leading-5
-                  text-gray-600
-                "
-              >
-                {{ estimate.estimate_number }}
-              </div>
-
-              <BaseEstimateStatusBadge
-                :status="estimate.status"
-                class="px-1 text-xs"
-              >
-                {{ estimate.status }}
-              </BaseEstimateStatusBadge>
-            </div>
-
-            <div class="flex-1 whitespace-nowrap right">
-              <BaseFormatMoney
-                :amount="estimate.total"
-                :currency="estimate.customer.currency"
-                class="
-                  block
-                  mb-2
-                  text-xl
-                  not-italic
-                  font-semibold
-                  leading-8
-                  text-right text-gray-900
-                "
-              />
-
-              <div
-                class="
-                  text-sm
-                  not-italic
-                  font-normal
-                  leading-5
-                  text-right text-gray-600
-                  est-date
-                "
-              >
-                {{ estimate.formatted_estimate_date }}
-              </div>
-            </div>
-          </router-link>
-        </div>
-        <div v-if="isLoading" class="flex justify-center p-4 items-center">
-          <LoadingIcon class="h-6 m-1 animate-spin text-primary-400" />
-        </div>
-        <p
-          v-if="!estimateList?.length && !isLoading"
-          class="flex justify-center px-4 mt-5 text-sm text-gray-600"
+    <div class="relative table-container pt-5">
+        <BaseTable
+          :data="estimateData.items"
+          :columns="itemsColumns"
+          :loading="isLoadingEstimate"
         >
-          {{ $t('estimates.no_matching_estimates') }}
-        </p>
-      </div>
+         
+        </BaseTable>
     </div>
 
-    <div
+
+    <!-- <div
       class="flex flex-col min-h-0 mt-8 overflow-hidden"
       style="height: 75vh"
     >
@@ -272,7 +60,7 @@
           frame-style
         "
       />
-    </div>
+    </div> -->
   </BasePage>
 </template>
 
@@ -290,6 +78,7 @@ import { useUserStore } from '@/scripts/admin/stores/user'
 import EstimateDropDown from '@/scripts/admin/components/dropdowns/EstimateIndexDropdown.vue'
 import SendEstimateModal from '@/scripts/admin/components/modal-components/SendEstimateModal.vue'
 import LoadingIcon from '@/scripts/components/icons/LoadingIcon.vue'
+import BaseCard from '@/scripts/components/base/BaseCard.vue'
 
 import abilities from '@/scripts/admin/stub/abilities'
 
@@ -318,7 +107,38 @@ const searchData = reactive({
   searchText: null,
 })
 
-const pageTitle = computed(() => estimateData.value.estimate_number)
+const itemsColumns = computed(() => {
+  return [
+    {
+      key: 'key',
+      label: "Partida",
+    },
+    {
+      key: 'name',
+      label: "Producto",
+    },
+    {
+      key: 'quantity',
+      label: "Litros",
+    },
+    {
+      key: 'precision_price',
+      label: "Precio Unitario",
+    },
+    {
+      key: 'total',
+      label: "Total",
+    },
+    {
+      key: 'actions',
+      tdClass: 'text-right text-sm font-medium pl-0',
+      thClass: 'text-right pl-0',
+      sortable: false,
+    },
+  ]
+})
+
+const pageTitle = computed(() => 'Previa: ' + estimateData.value.estimate_number)
 
 const getOrderBy = computed(() => {
   if (searchData.orderBy === 'asc' || searchData.orderBy == null) {
